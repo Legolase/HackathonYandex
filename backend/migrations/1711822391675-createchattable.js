e31dc07126e42f1db3328cffc699800994ef203d
IlyaStepanov1104 <ilyahtml@gmai.com>
2024-03-31 00:34:45 +0300

    add migrations

---
+++ backend/migrations/1711822391675-createchattable.js
@@ -0,0 +1,34 @@
+exports.up = (pgm) => {
+  pgm.createType('chat_type', ['single', 'multi', 'channel']);
+
+  pgm.createTable('chats', {
+    id: {
+      type: 'serial',
+      primaryKey: true
+    },
+    datetime: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp')
+    },
+    type: {
+      type: 'chat_type',
+      notNull: true
+    },
+    pin_message: {
+      type: 'integer'
+    },
+    name: {
+      type: 'varchar(100)',
+      notNull: true
+    },
+    avatar: {
+      type: 'text'
+    }
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('chats');
+  pgm.dropType('chat_type');
+};
--- backend/models/Model.ts
+++
@@ -1,6 +0,0 @@
-export class Model {
-    id: number = 0
-    date_create: string = '';
-    date_update: string = '';
-    table: string = '';
-}
