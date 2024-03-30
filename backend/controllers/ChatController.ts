bbdb200dc049f6c40d9d0723d36dd14bb7094eab
IlyaStepanov1104 <ilyahtml@gmai.com>
2024-03-30 15:19:46 +0300

    add simple MVC

---
+++ backend/controllers/ChatController.ts
@@ -0,0 +1,81 @@
+export const ChatController = {
+    'getList': () => {
+        return {
+            "chats": [
+                {
+                    "id": 1,
+                    "name": "chat_name 1",
+                    "avatar": "/storage/avatar.jpg",
+                    "last_message": {
+                        "id": 1,
+                        "text": "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                        "datetime": "2024-03-30T08:35:40.742Z",
+                        "from": 1,
+                        "read": true
+                    }
+                },
+                {
+                    "id": 2,
+                    "name": "chat_name 2",
+                    "avatar": "/storage/avatar.jpg",
+                    "last_message": {
+                        "id": 1,
+                        "text": "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                        "datetime": "2024-03-30T08:35:40.742Z",
+                        "from": 1,
+                        "read": true
+                    }
+                },
+                {
+                    "id": 3,
+                    "name": "chat_name 3",
+                    "avatar": "/storage/avatar.jpg",
+                    "last_message": {
+                        "id": 1,
+                        "text": "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                        "datetime": "2024-03-30T08:35:40.742Z",
+                        "from": 1,
+                        "read": true
+                    }
+                }
+            ]
+        };
+    },
+    'getItem': (id: string) => {
+        return {
+            "id": 1,
+            "name": "chat_name 1",
+            "avatar": "/storage/avatar.jpg",
+            "messages": [
+                {
+                    "id": 1,
+                    "text": "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                    "datetime": "2024-03-30T08:00:40.742Z",
+                    "from": 1,
+                    "read": true
+                },
+                {
+                    "id": 2,
+                    "text": "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                    "datetime": "2024-03-30T08:05:40.742Z",
+                    "from": 2,
+                    "read": true
+                },
+                {
+                    "id": 3,
+                    "text": "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                    "datetime": "2024-03-30T08:10:40.742Z",
+                    "from": 1,
+                    "read": true
+                },
+                {
+                    "id": 4,
+                    "text": "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+                    "datetime": "2024-03-30T08:15:40.742Z",
+                    "from": 2,
+                    "read": true
+                }
+            ]
+        };
+    },
+}
