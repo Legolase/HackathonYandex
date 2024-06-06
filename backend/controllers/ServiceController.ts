f5f7121d4a4cbf12028fa9545b91df3f4ac3c89a
IlyaStepanov1104 <ilyahtml@gmai.com>
2024-06-07 00:37:39 +0300

    add generate image method

--- backend/controllers/ServiceController.ts
+++ backend/controllers/ServiceController.ts
@@ -1,5 +1,8 @@
 import {s3} from "../index";
 import {UploadedFile} from "express-fileupload";
+import {Identicon} from "../facades/Identicon";
+import {v4 as uuidv4} from "uuid";
+import {Chat} from "../models/Chat";
 
 export const ServiceController = {
     async uploadFile(file: UploadedFile) {
@@ -10,5 +13,12 @@ export const ServiceController = {
         res.link = (await s3.uploadFile(file.data, '/uploads/')).Location;
         res.name = file.name;
         return res;
+    },
+    async generateImage(uid: string) {
+        let fileBuffer = Identicon.generateImage(uid + uuidv4());
+        let filePath = await s3.uploadFile(fileBuffer, '/generated/');
+        return {
+            link: filePath?.Location
+        };
     }
 }
